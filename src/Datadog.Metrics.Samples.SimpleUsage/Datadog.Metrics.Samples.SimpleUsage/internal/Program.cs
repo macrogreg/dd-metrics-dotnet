ede8f6b0832a346e631c0232930b955901653a20
macrogreg <macrogreg@live.com>
2021-09-16 21:40:50 -0700

    Configure build folder structure and default code style rules.

--- src/Datadog.Metrics.Samples.SimpleUsage/Datadog.Metrics.Samples.SimpleUsage/internal/Program.cs
+++ src/Datadog.Metrics.Samples.SimpleUsage/Datadog.Metrics.Samples.SimpleUsage/internal/Program.cs
@@ -6,7 +6,7 @@ namespace Datadog.Metrics.Samples.SimpleUsage
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
